663365f5cb0553ba0b4f6289268bb99f957ed671
lolcott2002 <olcott.logan@gmail.com>
2025-04-29 21:59:48 -0600

    completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,25 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+      
+     {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        Console.Write("what is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("higher");
+            }
+            else
+            {
+                Console.WriteLine("lower");
+            }
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
