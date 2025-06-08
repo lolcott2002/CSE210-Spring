630b074d3d0a8419dd7f018e9fd59018248a7f9a
lolcott2002 <olcott.logan@gmail.com>
2025-06-07 18:10:08 -0600

    worked on prep assignemnt

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Logan Olcott", "multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Ly Olcott", "fractions", "8.7", "12-27");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Alice Olcott", "A Very Hungry Caterpillar", "Quantum Physics for Babies");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
