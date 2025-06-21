75ba08c2004a7b0acbbac60bbf3cd49d2eb807d1
lolcott2002 <olcott.logan@gmail.com>
2025-06-20 21:51:03 -0600

    finsihed prep 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shapenumber1 = new Square("violet", 3);
+        shapes.Add(shapenumber1);
+
+        Rectangle shapenumber2 = new Rectangle("indigo", 4, 5);
+        shapes.Add(shapenumber2);
+
+        Circle shapenumber3 = new Circle("teal", 6);
+        shapes.Add(shapenumber3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"this {color} colored shape has an area of {area} units squared.");
+        }
     }
 }
